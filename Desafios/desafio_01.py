6acb9e87d36ad688ba85bb5f55bcbbbed9f4904f
Alessandra Lucas <alessandra1702@hotmail.com>
2026-01-16 20:26:37 -0300

    Desafio 01

---
+++ Desafios/desafio_01.py
@@ -0,0 +1,6 @@
+print()
+print('Desafio: Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.')
+print()
+print('====== Desafio 01 ======')
+nome = input('Qual o seu nome? ')
+print('Olá', nome,'! Prazer em te conhecer!' )
