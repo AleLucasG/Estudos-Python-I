b29e50439bc9f4dbeed1f2b320aaf02da459fba1
Alessandra Lucas <alessandra1702@hotmail.com>
2026-01-16 20:43:40 -0300

    desafio 02

---
+++ Desafios/desafio_02.py
@@ -0,0 +1,11 @@
+print()
+print('Desafio: Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formartada.')
+print()
+print('====== Desafio 02 ======')
+dia = input('Dia: ')
+mes = input('Mes: ')
+ano = input ('Ano: ')
+print('Você nasceu no dia',dia,'de',mes,'de',ano,'. Correto?')
+
+
+
