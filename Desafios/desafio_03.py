5725b83b17dc3e87ef202c81c28c32184f184b24
Alessandra Lucas <alessandra1702@hotmail.com>
2026-02-02 19:13:40 -0300

    dando contexto para desafio 03

--- Desafios/desafio_03.py
+++ Desafios/desafio_03.py
@@ -2,6 +2,7 @@ print()
 print('Desafio: Crie um script Python que leia dois números e tente mostrar a soma entre eles.')
 print()
 print('====== Desafio 03 ======')
+print('Digite dois números para ver a soma entre eles:')
 valor1 = int(input('Primeiro número: '))
 valor2 = int(input('Segundo valor: '))
 soma = valor1 + valor2
