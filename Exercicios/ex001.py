7bbbad948bc217b363f80af14efa75a952767d1e
Alessandra Lucas <alessandra1702@hotmail.com>
2026-02-02 19:53:41 -0300

    Ex 001

--- Exercicios/ex001.py
+++ Exercicios/ex001.py
@@ -2,3 +2,7 @@ print()
 print('Desafio 001: Crie um programa que escreve: "Olá, mundo!" na tela ')
 print()
 print('Olá, mundo!')
+print()
+print('Variável recebendo valor: ')
+msg = 'Olá, mundo!'
+print(msg)
