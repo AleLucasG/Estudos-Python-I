890d00f1cd258f0aa2505c21c33280cb3ab995f9
Alessandra Lucas <alessandra1702@hotmail.com>
2026-02-02 20:52:44 -0300

    Testando formas de impressão

---
+++ Testes/aula06a.py
@@ -0,0 +1,16 @@
+print('Testando formas de impressão')
+
+nome = 'Alessandra'
+idade = 28
+print(type(idade))
+
+print('Meu nome é {} e tenho {} anos'.format(nome, idade))
+print('Tenho {0} anos e me chamo {1}'.format(28, 'Alessandra'))
+print(f'Meu nome é {nome} e tenho {idade} anos')
+print()
+
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+soma = n1 + n2
+print('A soma entre {} e {} é igual a: {}'.format(n1, n2, soma))
+print('A soma entre {0} e {1} é igual a: {2}'.format(n1, n2, soma))
