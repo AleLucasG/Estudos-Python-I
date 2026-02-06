6355704d550654555a66f612d98fd9fc68778977
Alessandra Lucas <alessandra1702@hotmail.com>
2026-02-06 18:42:42 -0300

    Ex003
    Desafio 003: Crie um programa que leia dois números e mostre a soma entre eles.

---
+++ Exercicios/ex003.py
@@ -0,0 +1,8 @@
+print()
+print('Desafio 003: Crie um programa que leia dois números e mostre a soma entre eles.')
+print()
+valor1 = int(input('Informe o primeiro valor: '))
+valor2 = int(input('Informe o segundo valor: '))
+soma = valor1 + valor2
+print(f'A soma entre {valor1} e {valor2} é',soma)
+print(f'A soma entre {0} e {1} é {2}'.format(valor1, valor2, soma))
