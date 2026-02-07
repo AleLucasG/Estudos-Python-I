41e4c7e23364117285efbc63463caa7e7f375e9f
Alessandra Lucas <alessandra1702@hotmail.com>
2026-02-07 20:39:52 -0300

    ex006
    Desafio 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.

---
+++ Exercicios/ex006.py
@@ -0,0 +1,10 @@
+print()
+print('Desafio 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.')
+print()
+valor = int(input('Digite um valor: '))
+dobro = valor * 2
+triplo = valor * 3
+raiz = valor ** (1/2)
+print('O dobro de {0} é {1}. O triplo de {0} é {2} e a raiz quadrada de {0} é {3}'.format(valor, dobro, triplo, raiz, raiz))
+print('O dobro de {0} é {1}. O triplo de {0} é {2} e a raiz quadrada de {0} é {3:.2f}'
+      .format(valor, dobro, triplo, raiz, raiz))
