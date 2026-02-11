e3eeb81a2158a298d2af2339961c1fcabbc2b611
Alessandra Lucas <alessandra1702@hotmail.com>
2026-02-11 19:17:53 -0300

    ex008 com novas variaveis

--- Exercicios/ex008.py
+++ Exercicios/ex008.py
@@ -1,12 +1,23 @@
 print()
-print('Desafio 008: Escreva um programa que leia um valor em metros e o exiba convertido em centimentros e milimetros.')
-print('Escala: km → hm → dam → M → dm → cm → mm')
+print('Desafio 008: Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.')
+print('Escala: km → hm → dam → m → dm → cm → mm')
 print('Cada passo na escala vale 10 vezes.')
 print('Descendo a escala → multiplica por 10')
 print('Subindo a escala → divide por 10')
-
 print()
+
 metros = float(input('Digite um valor em metros: '))
-centi = metros * 100
-mili = metros * 1000
-print('{} metros, é igual a {}cm e {}mm.'.format(metros, centi, mili))
+dm = metros * 10
+cm = metros * 100
+mm = metros * 1000
+
+dam = metros / 10
+hm = metros / 100
+km = metros / 1000
+
+print('{} metros, é igual a {}dam, {}hm, {}km.'.format(metros, dam, hm, km))
+print('{} metros, é igual a {}dm, {}cm, {}mm.'.format(metros, dm, cm, mm))
+
+#com 2 casas decimais
+print(f'{metros:.2f} m = {km:.2f} km, {hm:.2f} hm, {dam:.2f} dam')
+print(f'{metros:.2f} m = {dm:.2f} dm, {cm:.2f} cm, {mm:.2f} mm')
