9fb6d12185348b62a081bcb102dc420486300fbb
Alessandra Lucas <alessandra1702@hotmail.com>
2026-02-11 19:37:26 -0300

    ex009
    Exercício Python 9: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.

---
+++ Exercicios/ex009.py
@@ -0,0 +1,43 @@
+print()
+print('Desafio 009: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada. .')
+print()
+
+valor = int(input('Digite um valor para calcular a tabuada: '))
+print('{} x 0 = {}'.format(valor, valor * 0))
+print('{} x 1 = {}'.format(valor, valor * 1))
+print('{} x 2 = {}'.format(valor, valor * 2))
+print('{} x 3 = {}'.format(valor, valor * 3))
+print('{} x 4 = {}'.format(valor, valor * 4))
+print('{} x 5 = {}'.format(valor, valor * 5))
+print('{} x 6 = {}'.format(valor, valor * 6))
+print('{} x 7 = {}'.format(valor, valor * 7))
+print('{} x 8 = {}'.format(valor, valor * 8))
+print('{} x 9 = {}'.format(valor, valor * 9))
+print('{} x 10 = {}'.format(valor, valor * 10))
+print()
+
+# com variável
+valor0 = valor * 0
+valor1 = valor * 1
+valor2 = valor * 2
+valor3 = valor * 3
+valor4 = valor * 4
+valor5 = valor * 5
+valor6 = valor * 6
+valor7 = valor * 7
+valor8 = valor * 8
+valor9 = valor * 9
+valor10 = valor * 10
+
+print('{} x 0 = {}'.format(valor, valor0))
+print('{} x 1 = {}'.format(valor, valor1))
+print('{} x 2 = {}'.format(valor, valor2))
+print('{} x 3 = {}'.format(valor, valor3))
+print('{} x 4 = {}'.format(valor, valor4))
+print('{} x 5 = {}'.format(valor, valor5))
+print('{} x 6 = {}'.format(valor, valor6))
+print('{} x 7 = {}'.format(valor, valor7))
+print('{} x 8 = {}'.format(valor, valor8))
+print('{} x 9 = {}'.format(valor, valor9))
+print('{} x 10 = {}'.format(valor, valor10))
+
