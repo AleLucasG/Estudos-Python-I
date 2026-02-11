39f02051ecadf0feac47dd12324e86367917962d
Alessandra Lucas <alessandra1702@hotmail.com>
2026-02-11 18:57:44 -0300

    ex007 ajustado

--- Exercicios/ex007.py
+++ Exercicios/ex007.py
@@ -1,8 +1,8 @@
 print()
 print('Desafio 007: Desenvolva um programa que leia as duas notas de uma aluno, calcule e mostre a sua média.')
 print()
-nota1 = float(input('Primeiro valor: '))
-nota2 = float(input('Segundo valor: '))
-cal = (nota1 + nota2) / 2
-print('A média do aluno é {}'.format(cal))
-print('A média entre {:.1f} e {:.2f} é igual a {:.1f}' .format(nota1, nota2, (nota1+nota2)/2))
+nota1 = float(input('Primeiro nota: '))
+nota2 = float(input('Segundo nota: '))
+media = (nota1 + nota2) / 2
+print('A média do aluno é {}'.format(media))
+print('A média entre as notas {:.1f} e {:.2f} é igual a {:.1f}' .format(nota1, nota2, (nota1+nota2)/2))
