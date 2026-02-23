76d0034fc9bec66be764856fe368a53befc753e5
Alessandra Lucas <alessandra1702@hotmail.com>
2026-02-23 19:26:55 -0300

    ex022
    desafio extra

--- Exercicios/ex022.py
+++ Exercicios/ex022.py
@@ -1,10 +1,12 @@
 print()
 print('Desafio 22: Crie um programa que leia o nome completo de uma pessoa e mostre: \n'
       '– O nome com todas as letras maiúsculas e minúsculas.\n'
-      '– Quantas letras ao todo (sem considerar espaços).')
+      '– Quantas letras ao todo (sem considerar espaços).\n'
+      '- Extra: Quantas letras tem o primeiro nome.')
 print()
 
-nome = str(input('Digite seu nome completo: '))
-print(f'{nome.upper()}')
-print(f'{nome.lower()}')
-print(f'{len(nome) - nome.count(' ')} letras')
+nome = str(input('Digite seu nome completo: ')).strip()
+print(f'O nome com todas as letras maiúsculas: {nome.upper()}')
+print(f'O nome com todas as letras minúsculas: {nome.lower()}')
+print(f'Quantas letras ao todo: {len(nome) - nome.count(' ')} letras')
+print(f'Quantas letras tem o primeiro nome: {nome.find(' ')} letras ')
