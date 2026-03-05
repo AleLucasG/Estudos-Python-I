30b82163fb58d6e3fe9fc1e8cce42553329e2c59
Alessandra Lucas <alessandra1702@hotmail.com>
2026-03-05 18:32:45 -0300

    ex032
    Exercício Python 32: Faça um programa que leia um ano qualquer e mostre se ele é bissexto.

---
+++ Exercicios/ex032.py
@@ -0,0 +1,14 @@
+print()
+print('Desafio 32: Faça um programa que leia um ano qualquer e mostre se ele é bissexto.')
+print('Formula: (Ano % 4 == 0 AND Ano % 100 != 0) OR (Ano % 400 == 0)')
+
+print('========= VERIFICANDO SE O ANO É BISSEXTO OU NÃO =========')
+print()
+ano = int(input('Digite o ano: '))
+
+if (ano % 4 == 0
+        and ano % 100 != 0
+        or ano % 400 == 0):
+    print(f'O ano {ano} é BISSEXTO.')
+else:
+    print(f'O ano {ano} é NÃO É BISSEXTO.')
