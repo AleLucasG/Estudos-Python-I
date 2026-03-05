a1c78ae86bc5eb65f16e9a46311a50bb02edfd69
Alessandra Lucas <alessandra1702@hotmail.com>
2026-03-05 18:40:42 -0300

    ex032
    selecionando ano do sistema.

--- Exercicios/ex032.py
+++ Exercicios/ex032.py
@@ -2,9 +2,15 @@ print()
 print('Desafio 32: Faça um programa que leia um ano qualquer e mostre se ele é bissexto.')
 print('Formula: (Ano % 4 == 0 AND Ano % 100 != 0) OR (Ano % 400 == 0)')
 
+import datetime
+
 print('========= VERIFICANDO SE O ANO É BISSEXTO OU NÃO =========')
 print()
 ano = int(input('Digite o ano: '))
+
+# Vericano o ano atual
+#if ano == 0:
+    #ano = datetime.date.today().year
 
 if (ano % 4 == 0
         and ano % 100 != 0
